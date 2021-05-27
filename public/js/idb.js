688749b2484d6780f80478b1fc879239d99cd453
Ryobia <ryobia36@gmail.com>
2021-05-27 17:00:16 -0600

    idb.js

---
+++ public/js/idb.js
@@ -0,0 +1,71 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("new_budget", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    uploadBudget();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["new_budget"], "readwrite");
+
+  const budgetObjectStore = transaction.objectStore("new_budget");
+
+  budgetObjectStore.add(record);
+}
+
+function uploadBudget() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+  
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_budget');
+  
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+  
+    getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+          fetch('/api/transaction', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(response => response.json())
+            .then(serverResponse => {
+              if (serverResponse.message) {
+                throw new Error(serverResponse);
+              }
+              // open one more transaction
+              const transaction = db.transaction(['new_budget'], 'readwrite');
+              // access the new_budget object store
+              const budgetObjectStore = transaction.objectStore('new_budget');
+              // clear all items in your store
+              budgetObjectStore.clear();
+    
+              alert('All saved budgets have been submitted!');
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      };
+  };
+
+  window.addEventListener('online', uploadPizza);
